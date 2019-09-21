5087e68640069be9cc8ec9d54057ccfdffcd556f
Spencer Takata <stakata@google.com>
2019-09-20 21:23:20 -0700

    unneded code that's just food for thought - love, alexa

--- first-express-app/app.js
+++ first-express-app/app.js
@@ -1,16 +1,39 @@
-var express = require('express');
+const express = require('express');
 var app = express();
+const http = require('https');
+const hostname = 'localhost';
+const port = 9080;
+const server = https.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
 
 // "/" => "Hi there!"
 app.get("/", function(req, res){
 	res.send("Hi there");
 });
-
 // "/bye" => "Goodbye"
-app.get("/bye", function(req, res){
+app.get("/bye/", function(req, res){
 	res.send("Goodbye");
 });
 // "/dog" => "Meow"
+app.get("/dog", function(req, res){
+	res.send("Woof");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
 
 //Tell express to listen
 app.listen(process.env.PORT, process.env.IP, function(){
